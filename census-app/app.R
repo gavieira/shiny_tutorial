7216e1489a8482771952fa943fc1bfda6d5ff147
gavieira <gabrieldeusdeth@gmail.com>
2023-09-19 15:34:38 -0300

    Finished lesson 4

---
+++ census-app/app.R
@@ -0,0 +1,43 @@
+ui <- fluidPage(
+  titlePanel("censusVis"),
+
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with
+               information from the 2010 US Census."),
+
+      selectInput("var",
+                  label = "Choose a variable to display",
+                  choices = c("Percent White",
+                              "Percent Black",
+                              "Percent Hispanic",
+                              "Percent Asian"),
+                  selected = "Percent White"),
+
+      sliderInput("range",
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+    ),
+
+    mainPanel(
+      textOutput("selected_var"),
+      textOutput("selected_range")
+    )
+  )
+)
+
+
+server <- function(input, output) {
+
+  output$selected_var <- renderText({
+    paste("You have selected", input$var)
+  })
+
+  output$selected_range <- renderText({
+    paste("You have selected a range that goes from", min(input$range), 'to', max(input$range))
+  })
+
+}
+
+shinyApp(ui = ui, server = server)
+
