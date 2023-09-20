065152bef80c18fb0b71be83271c1ac44f511758
gavieira <gabrieldeusdeth@gmail.com>
2023-09-19 21:38:22 -0300

    Finished lesson 9

--- movie_browser/app.R
+++ movie_browser/app.R
@@ -1,23 +1,15 @@
-# Load packages
+# Load packages ----------------------------------------------------------------
 
 library(shiny)
 library(ggplot2)
 
-# Get the data
-
-file <- "https://github.com/rstudio-education/shiny-course/raw/main/movies.RData"
-destfile <- "movies.RData"
-
-download.file(file, destfile)
-
-# Load data
+# Load data --------------------------------------------------------------------
 
 load("movies.RData")
 
-# Define UI
+# Define UI --------------------------------------------------------------------
 
 ui <- fluidPage(
-
   sidebarLayout(
 
     # Inputs: Select variables to plot
@@ -27,33 +19,41 @@ ui <- fluidPage(
       selectInput(
         inputId = "y",
         label = "Y-axis:",
-        choices = c("IMDB Rating" = "imdb_rating",
-                    "IMDB Number of Votes" = "imdb_num_votes",
-                    "Critics Score" = "critics_score",
-                    "Audience Score" = "audience_score",
-                    "Runtime" = "runtime"),
+        choices = c(
+          "IMDB rating" = "imdb_rating",
+          "IMDB number of votes" = "imdb_num_votes",
+          "Critics score" = "critics_score",
+          "Audience score" = "audience_score",
+          "Runtime" = "runtime"
+        ),
         selected = "audience_score"
       ),
+
       # Select variable for x-axis
       selectInput(
         inputId = "x",
         label = "X-axis:",
-        choices = c("IMDB Rating" = "imdb_rating",
-                    "IMDB Number of Votes" = "imdb_num_votes",
-                    "Critics Score" = "critics_score",
-                    "Audience Score" = "audience_score",
-                    "Runtime" = "runtime"),
+        choices = c(
+          "IMDB rating" = "imdb_rating",
+          "IMDB number of votes" = "imdb_num_votes",
+          "Critics score" = "critics_score",
+          "Audience score" = "audience_score",
+          "Runtime" = "runtime"
+        ),
         selected = "critics_score"
       ),
-      # Select variable to color by
+
+      # Select variable for color
       selectInput(
         inputId = "z",
         label = "Color by:",
-        choices = c("Title Type" = "title_type",
-                    "Genre" = "genre",
-                    "MPAA Rating" = "mpaa_rating",
-                    "Critics Rating" = "critics_rating",
-                    "Audience Rating" = "audience_rating"),
+        choices = c(
+          "Title type" = "title_type",
+          "Genre" = "genre",
+          "MPAA rating" = "mpaa_rating",
+          "Critics rating" = "critics_rating",
+          "Audience rating" = "audience_rating"
+        ),
         selected = "mpaa_rating"
       )
     ),
@@ -65,15 +65,18 @@ ui <- fluidPage(
   )
 )
 
-# Define server
+# Define server ----------------------------------------------------------------
 
 server <- function(input, output, session) {
   output$scatterplot <- renderPlot({
+
     ggplot(data = movies, aes_string(x = input$x, y = input$y, color = input$z)) +
       geom_point()
+
   })
+
 }
 
-# Create a Shiny app object
+# Create a Shiny app object ----------------------------------------------------
 
 shinyApp(ui = ui, server = server)
